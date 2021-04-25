ed446118fb643448c24034b316d5e3b58c52a8db
shinoairisu <37396293+shinoairisu@users.noreply.github.com>
2021-04-25 20:45:55 +0900

    Add files via upload

---
+++ tools/common_tools.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+
+import torch
+import random
+import psutil
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
+
+
+def transform_invert(img_, transform_train):
+    """
+    将data 进行反transfrom操作
+    :param img_: tensor
+    :param transform_train: torchvision.transforms
+    :return: PIL image
+    """
+    if 'Normalize' in str(transform_train):
+        norm_transform = list(filter(lambda x: isinstance(x, transforms.Normalize), transform_train.transforms))
+        mean = torch.tensor(norm_transform[0].mean, dtype=img_.dtype, device=img_.device)
+        std = torch.tensor(norm_transform[0].std, dtype=img_.dtype, device=img_.device)
+        img_.mul_(std[:, None, None]).add_(mean[:, None, None])
+
+    img_ = img_.transpose(0, 2).transpose(0, 1)  # C*H*W --> H*W*C
+    if 'ToTensor' in str(transform_train) or img_.max() < 1:
+        img_ = img_.detach().numpy() * 255
+
+    if img_.shape[2] == 3:
+        img_ = Image.fromarray(img_.astype('uint8')).convert('RGB')
+    elif img_.shape[2] == 1:
+        img_ = Image.fromarray(img_.astype('uint8').squeeze())
+    else:
+        raise Exception("Invalid img shape, expected 1 or 3 in axis 2, but got {}!".format(img_.shape[2]) )
+
+    return img_
+
+
+def set_seed(seed=1):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+
+
+def get_memory_info():
+    virtual_memory = psutil.virtual_memory()
+    used_memory = virtual_memory.used/1024/1024/1024
+    free_memory = virtual_memory.free/1024/1024/1024
+    memory_percent = virtual_memory.percent
+    memory_info = "Usage Memory：{:.2f} G，Percentage: {:.1f}%，Free Memory：{:.2f} G".format(
+        used_memory, memory_percent, free_memory)
+    return memory_info
+
+#语义分割调色板生成器
+def palettemaker(num_of_color):
+    palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+    colors = torch.as_tensor([i for i in range(num_of_color)])[:, None] * palette
+    colors = (colors % 255).numpy().astype("uint8")
+    return colors
+
+
+
+
+
---
+++ tools/readme.md
@@ -0,0 +1,7 @@
+# 实用工具系列
+
+PILTareader.py  --把tar当二进制数据集读的程序
+
+Lossholder.py --第一版本的神经网络参数管理程序
+
+common_tools --可以将tensor化且已经被正则化的图片还原回原图
